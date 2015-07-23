b364666966abc91bc205d8269bb4573d683ef720
endhrk <endhrk@gmail.com>
2015-07-24 08:18:21 +0900

    change regex

--- lib/fluent/plugin/in_cloudwatch_logs.rb
+++ lib/fluent/plugin/in_cloudwatch_logs.rb
@@ -139,8 +139,8 @@ module Fluent
           router.emit(@tag, record[0], record[1])
         else
           time = (event.timestamp / 1000).floor
-          /{.*}/ =~ event.message
-          record = JSON.parse($&)
+          /(\{.*\})/ =~ event.message
+          record = JSON.parse($1)
           router.emit(@tag, time, record)
         end
       end
